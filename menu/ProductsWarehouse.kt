0523589cb3d6d8688313d59fe859efd9ee3bb4b1
Vaibhav Kohli <68368418+kohli2113@users.noreply.github.com>
2023-11-04 18:25:13 +0530

    Add files via upload

---
+++ menu/ProductsWarehouse.kt
@@ -0,0 +1,20 @@
+package com.littlelemon.menu
+
+object ProductsWarehouse {
+    val productsList = mutableListOf(
+        ProductItem("Qatar Airways", 3000.00, "First Class", R.drawable.qatar),
+        ProductItem("Air Asia", 3500.00, "First Class", R.drawable.airasia),
+        ProductItem("Air India", 5200.00, "First Class", R.drawable.indian),
+        ProductItem("Indigo", 8400.00, "First Class", R.drawable.indigo),
+        ProductItem("Lufthansa", 8900.00, "First Class", R.drawable.lufthansa),
+        ProductItem("Spicejet", 1000.00, "First Class", R.drawable.spicejet),
+        ProductItem("Vistara", 1500.00, "Economy", R.drawable.vistara),
+        ProductItem("Emirates", 2000.00, "Economy", R.drawable.emirates),
+        ProductItem("Baltic Airways", 1500.00, "Economy", R.drawable.baltic),
+        ProductItem("Delta", 1200.00, "Economy", R.drawable.delta),
+        ProductItem("Turkish Airlines", 2500.00, "Economy", R.drawable.turkish),
+        ProductItem("Swissair", 1700.00, "Business", R.drawable.swiss),
+        ProductItem("Air Mauritius", 1400.00, "Business", R.drawable.mauritius),
+        ProductItem("British Airways", 6700.00, "Business", R.drawable.british),
+    )
+}
